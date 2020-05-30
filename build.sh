a9fbe274f9b4f1271bff9b228b831634591270dc
Alex Pardoe <alex@pardoe.io>
2020-05-30 16:17:03 +0100

    Updated build script.

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+readonly OUTPUT="pocket-youtube-dl.tar"
+
+rm $OUTPUT
 docker build -t digitalpardoe/pocket-youtube-dl:latest .
-docker save digitalpardoe/pocket-youtube-dl:latest > pocket-youtube-dl.tar
+docker save digitalpardoe/pocket-youtube-dl:latest > $OUTPUT
